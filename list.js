f7a4999593fd6962afdbe6e7526e1af1eb33e56f
Tomas Stropus <tomas.stropus@devbridge.com>
2017-06-01 13:57:33 +0300

    Improved layout

--- list.js
+++ list.js
@@ -3,30 +3,30 @@ var items = [
     "name": "Facebook",
     "icon": "facebook.svg",
     "link": "https://www.facebook.com",
-    "shortcut": 70
+    "shortcut": 70 // F
   },
   {
     "name": "Quora",
     "icon": "quora.svg",
     "link": "https://www.quora.com",
-    "shortcut": 81
+    "shortcut": 81 // Q
   },
   {
     "name": "Youtube",
     "icon": "youtube.svg",
     "link": "https://www.youtube.com",
-    "shortcut": 89
+    "shortcut": 89 // Y
   },
   {
     "name": "Reddit",
     "icon": "reddit.svg",
     "link": "https://www.reddit.com",
-    "shortcut": 82
+    "shortcut": 82 // R
   },
   {
     "name": "Google+",
     "icon": "googleplus.svg",
     "link": "https://plus.google.com",
-    "shortcut": 71
+    "shortcut": 71 // G
   }
 ];
